048a6cf590b0a91aa5a3f3d7175fe8102796fe87
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2022-01-28 17:43:07 -0300

    [catalog] - creating filters panel initial structure

---
+++ packages/catalog/src/components/Filters/FiltersPanel.styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.aside`
+  background: #fff;
+  border: 1px solid #000;
+  border-radius: 0;
+  display: flex;
+  flex-direction: column;
+  padding: 1em;
+  width: 300px;
+`;
+
+export const FilterSection = styled.section`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const FilterSectionTitle = styled.h3`
+  font-size: 20px;
+  font-weight: bold;
+  margin: 0;
+  margin-bottom: 10px;
+`;
---
+++ packages/catalog/src/components/Filters/FiltersPanel.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import {
+  Container,
+  FilterSection,
+  FilterSectionTitle,
+} from "./FiltersPanel.styles";
+
+interface Category {
+  id: number;
+  name: string;
+}
+
+interface Filters {
+  category?: number[];
+}
+
+const FiltersPanel = () => {
+  const [categories, setCategories] = React.useState<Category[]>([
+    {
+      id: Math.random(),
+      name: "Hardware",
+    },
+    {
+      id: Math.random(),
+      name: "Games",
+    },
+    {
+      id: Math.random(),
+      name: "Eletronics",
+    },
+  ]);
+
+  const [filters, setFilters] = React.useState<Filters>({
+    category: []
+  });
+
+  const formRef = React.useRef<HTMLFormElement>(null);
+
+  const handleFilterChanges = (event: { target: any }) => {
+    const { target } = event;
+    const { value, name } = target as HTMLInputElement;
+
+    if (name === "category") {
+      setFilters((prev) => {
+        const { category } = prev;
+        if (category.includes(Number(value))) {
+          return {
+            ...prev,
+            [name]: category.filter((el) => el !== Number(value)),
+          };
+        }
+
+        return {
+          ...prev,
+          [name]: [...category, Number(value)],
+        };
+      });
+    }
+  };
+
+  React.useEffect(() => {
+    console.log('filters :>> ', filters);
+  }, [filters]);
+
+  return (
+    <Container>
+      <form ref={formRef}>
+        <FilterSection>
+          <FilterSectionTitle>Categories</FilterSectionTitle>
+          {categories.map((category) => (
+            <label
+              htmlFor={category.id.toString()}
+              key={category.id.toString()}
+            >
+              <input
+                type="checkbox"
+                name="category"
+                id={category.id.toString()}
+                value={category.id}
+                onChange={handleFilterChanges}
+              />
+              <span>{category.name}</span>
+            </label>
+          ))}
+        </FilterSection>
+      </form>
+    </Container>
+  );
+};
+
+export default FiltersPanel;
